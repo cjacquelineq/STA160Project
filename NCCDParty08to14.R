ce95557a49bf04cc7960fd280f500ffc46f92d95
cjacquelineq <cjacquelineq@gmail.com>
2017-06-12 11:43:37 -0700

    update

--- NCCDParty08to14.R
+++ NCCDParty08to14.R
@@ -2,6 +2,7 @@ library(sp)
 library(tigris)
 library(rgdal)
 library(readr)
+library(htmlwidgets)
 
 # convert into binary values based on party: D = 0(Blue), R = 1 (Red)
 district = c('01','02','03','04','05','06','07','08','09','10','11','12','13')
@@ -16,7 +17,7 @@ NC_2014 = data.frame('district'=district,'party' = NC_CD14_party_bi )
 
 NC_2008_shp = readOGR("/Users/chenjingqi/Dropbox/College/2017 Spring/STA 160/Gerrymandering/Congressional Districts/NC_Shapefiles/cd37_110.shp")
 US_2014_shp = readOGR('/Users/chenjingqi/Dropbox/College/2017 Spring/STA 160/Gerrymandering/Congressional Districts/Shapefiles/cb_2013_us_cd113_500k/cb_2013_us_cd113_500k.shp')
-NC_2014_shp = US_2014[US_2014$STATEFP == 37,]
+NC_2014_shp = US_2014_shp[US_2014_shp$STATEFP == 37,]
 
 cds = congressional_districts()
 #cds2011 = congressional_districts(year = 2013)
@@ -33,7 +34,7 @@ popup_08 <- paste0("<span style='color:#7f0000'><strong>
                    paste('Congressional District',as.character(NC_08_merged$CD)))
 
 pal <- colorNumeric(c('#67a9cf','#f7f7f7', '#ef8a62'), NULL, n=5)
-leaflet() %>%
+NC08 = leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(data = NC_08_merged, 
               fillColor = ~pal(NC_08_merged$party), 
@@ -54,14 +55,14 @@ leaflet() %>%
     overlayGroups = c("Party 2008"),
     options = layersControlOptions(collapsed = FALSE)
   )
-
+saveWidget(NC08, file="Congressional District in 2008 North Carolina.html")
 
 
 
 
 
 pal <- colorNumeric(c('#67a9cf','#f7f7f7', '#ef8a62'), NULL, n=5)
-leaflet() %>%
+NC14 = leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(data = NC_14_merged, 
               fillColor = ~pal(NC_14_merged$party), 
@@ -82,4 +83,4 @@ leaflet() %>%
     overlayGroups = c("Party 2014"),
     options = layersControlOptions(collapsed = FALSE)
   )
-
+saveWidget(NC14, file="Congressional District in 2014 North Carolina.html")
